fed251ab44605ef6d6768a2ad06bec1ea13d776b
bdnkai <bdn.kai@gmail.com>
2022-12-01 15:21:31 -0800

    reverse some refactoring, had with screen only capturing one adb

--- src/vision.py
+++ src/vision.py
@@ -4,49 +4,59 @@ import numpy as np
 
 class Vision:
 
-    # given a list of [x, y, w, h] rectangles returned by find(), convert those into a list of
-    # [x, y] positions in the center of those rectangles where we can click on those found items
-    def get_click_points(self, rectangles):
+    def __init__(self, img_path, method=cv.TM_CCOEFF_NORMED):
+        self._img = cv.imread(img_path, cv.IMREAD_UNCHANGED)
+
+        self._w = _img.shape[1]g
+        self._h = _img.shape[0]
+
+        self.method = method
+
+    def find(self, _img, threshold=0.8, debug=None):
+        result = cv.matchTemplate(_img, self._img, cv.TM_CCOEFF_NORMED)
+        print(result)
+
+        locations = np.where(result >= threshold)
+        locations = list(zip(*locations[::-1]))
+        print(locations)
+
+
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), self._w, self._h]
+            rectangles.append(rect)
+            print(rectangles)
+
+
+    def get_input_positions(self, rectangles):
         points = []
 
-        # Loop over all the rectangles
         for (x, y, w, h) in rectangles:
-            # Determine the center position
             center_x = x + int(w/2)
             center_y = y + int(h/2)
-            # Save the points
             points.append((center_x, center_y))
 
         return points
 
-    # given a list of [x, y, w, h] rectangles and a canvas image to draw on, return an image with
-    # all of those rectangles drawn
-    def draw_rectangles(self, haystack_img, rectangles):
-        # these colors are actually BGR
+    def draw_rectangles(self, _img, rectangles):
         line_color = (0, 255, 0)
         line_type = cv.LINE_4
 
         for (x, y, w, h) in rectangles:
-            # determine the box positions
             top_left = (x, y)
             bottom_right = (x + w, y + h)
-            # draw the box
-            cv.rectangle(haystack_img, top_left, bottom_right, line_color, lineType=line_type)
+            cv.rectangle(_img, top_left, bottom_right, line_color, lineType=line_type)
 
-        return haystack_img
+        return _img
 
-    # given a list of [x, y] positions and a canvas image to draw on, return an image with all
-    # of those click points drawn on as crosshairs
-    def draw_crosshairs(self, haystack_img, points):
-        # these colors are actually BGR
+    def draw_crosshairs(self, _img, points):
         marker_color = (255, 0, 255)
         marker_type = cv.MARKER_CROSS
 
         for (center_x, center_y) in points:
-            # draw the center point
-            cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marker_type)
+            cv.drawMarker(_img, (center_x, center_y), marker_color, marker_type)
 
-        return haystack_img
+        return _img
 
     def centeroid(self, point_list):
         point_list = np.asarray(point_list, dtype=np.int32)
