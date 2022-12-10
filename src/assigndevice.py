43e66918c2432b29be129d7abd89b7108f90501d
bdnkai <bdn.kai@gmail.com>
2022-12-09 20:36:59 -0800

    Refactored functions within main to be in assigndevice

--- src/assigndevice.py
+++ src/assigndevice.py
@@ -10,40 +10,54 @@ from windowcapture import WindowCapture
 
 
 class AssignDevice:
-#   properties
-    device = None
-    device_name = None
-    device_h = 0
-    device_w = 0
     adb = AdbClient(host='127.0.0.1', port=5037)
     devices = adb.devices()
+
+#   properties
+    namesArray = ["badbar0","badbar1","badbar2","badbar3"]
+    device = None
+    device_names = None
+    device_h = 0
+    device_w = 0
+
     screenshot = []
     vision_image = None
 
+
+
 #   constructor
     def __init__(self, device_name, vision_image):
-            if len(self.devices) >= 1:
+        if len(self.devices) >= 1:
+            for i in range(devices):
+                print(f'Im seeing {len(devices)} devices, currently assigning device:{i}')
                 for device in self.devices:
-                    self.device = _new_device
-                    self.device_name = self.device
-                    self.device_name = device_name
-                    self.device_name = self.device_name
-                    self.wincap = WindowCapture(device_name)
-
-                while True:
-                    self.screenshot = self.wincap.get_screenshot()
-                    # print('screenshot has been mutated to: ' f'{self.screenshot}')
-                    # print('vision image has been mutated to: ' f'{self.vision_image}')
-                    points = vision_image.find(self.screenshot,0.8, 'points')
-                    print(len(AssignDevice.devices))
-                    quiit()
-                    cv.imshow(f'{device_name}', self.screenshot)
-
-                    if cv.waitKey(1) == ord('q'):
-                        cv.destroyAllWindows()
-                        quit()
-                        print('Done.')
+                    if i == len(self.devices):
+                        print(f'No more devices to assing')
                         break
+                    elif i < len(self.devices):
+                        new_device = device[i]
+                        self.device = new_device
+                        self.device_name = self.device
+                        self.device_name = device_name
+                        self.device_name = self.device_name
+                        self.wincap = WindowCapture(device_name)
+                    else:
+                        print(i)
+
+                # while True:
+                #     self.screenshot = self.wincap.get_screenshot()
+                #     # print('screenshot has been mutated to: ' f'{self.screenshot}')
+                #     # print('vision image has been mutated to: ' f'{self.vision_image}')
+                #     points = vision_image.find(self.screenshot,0.8, 'points')
+                #     print(len(AssignDevice.devices))
+                #     quiit()
+                #     cv.imshow(f'{device_name}', self.screenshot)
+                #
+                #     if cv.waitKey(1) == ord('q'):
+                #         cv.destroyAllWindows()
+                #         quit()
+                #         print('Done.')
+                #         break
 
 
 
--- src/main.py
+++ src/main.py
@@ -11,41 +11,6 @@ from assigndevice import AssignDevice as aDevice
 os.path.abspath(__file__)
 
 
-# gnmenu_img = cv.imread('./img/gamenotice_img.png', cv.IMREAD_UNCHANGED)
-# gntitlebar_img = cv.imread('./img/gntitlebar.png', cv.IMREAD_UNCHANGED)
-
-
-
-
-# screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
-# result = cv.matchTemplate(gntitlebar_img, screenshot, cv.TM_CCOEFF_NORMED)
-# print(result)
-
-# threshold = 1
-# locations = np.where(result >= threshold)
-# locations = list(zip(*locations[::-1]))
-# print(locations)
-#
-# if locations:
-#     # print('found a match')
-#     gntitlebar_w = gntitlebar_img.shape[1]
-#     gntitlebar_h = gntitlebar_img.shape[0]
-#     line_color = (0,255,0)
-#     line_type = cv.LINE_4
-#
-#     for loc in locations:
-#         top_left = loc
-#         bottom_right = (top_left[0] + gntitlebar_w, top_left[1] + gntitlebar_h)
-#
-#         cv.rectangle(screenshot, top_left, bottom_right, line_color, line_type)
-# else:
-#   print('match not found')
-
-
-# wincap = WindowCapture("badbar0")
-# namesArray = ['badbar0']
-namesArray = ["badbar0","badbar1","badbar2","badbar3"]
-
 def img_path(pic_name):
     img = f'src//img//{pic_name}.png'
     return img
@@ -58,12 +23,12 @@ vision_gntitlebar = Vision(icon_img)
 
 currentDevice = []
 def android_device(vision_image):
-    for device in range(len(aDevice.devices)):
 
-        device_name = aDevice(namesArray[device],vision_image)
-        currentDevice.append(device_name)
-        println('current device has been pushed')
-        println(currentDevice)
+
+    device_name = aDevice(namesArray[device],vision_image)
+    currentDevice.append(device_name)
+    println('current device has been pushed')
+    println(currentDevice)
         # if statment for multiple device cvshow
 
 
